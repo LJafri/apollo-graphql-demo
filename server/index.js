3c25b5a15a0623587c046bc85ef62b85133c4dc8
ljafri <ljsfdcdev@gmail.com>
2022-11-10 09:43:10 -0700

    server:

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 4000;
-
+const { graphqlHTTP } = require('express-graphql');
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    
+}))
 
 app.listen(port, console.log(`Server running on port ${port}`));
